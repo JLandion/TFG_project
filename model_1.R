c6720f612155e509a45563aea31f16aeb98a89d2
Juan Luis Andión Tápiz <57728481+JLandion@users.noreply.github.com>
2020-10-28 12:03:28 +0000

    Subida de scripts modificados
    Subo la última versión de los scripts

--- model_1.R
+++ model_1.R
@@ -1,198 +1,158 @@
 #### PAQUETES USADOS####
 
 #install.packages("readxl")
-#install.packages("dplyr")
-#install.packages("openxlsx")
 #install.packages("mlogit")
 #install.packages("glm2")
 
 
-library(readxl)
-library(dplyr)
-library(openxlsx)
-library(glm2)
+
+
 # Importamos la base de datos limpia ####
+library(readxl)
 datos_def <- read_excel("datos_def_excel")
 names(datos_def)
 summary(datos_def)
+#options(digits = 4,scipen = 5)
 #### Analisis descriptivo ####
 
 
-#### M1: ESTIMACION DEL MODELO SIN CONSIDERAR LAS CARACTERISTICAS DE LA VIVIENDA ####
+
 
 # Introducimos las variables de interes un objeto subset para que puedan introducirse en el objeto glm
-datos_modelo <- subset(datos_def, select = c("SAP", "sexo", "edad","altura","peso", "niv_est","enf_cron","IMC","fuma", "sit_lab",
-                                             "frec_act_fis","estrato","ingreso","ingreso_eq","GHQ_12","m2","n_adultos","n_menores",
-                                            "ruido", "malos","agua","limpieza",
-                                            "cont_indus", "cont_otras","escasez_verde", "molest_animal", "delincuencia"))
-head(datos_modelo)
+datos_modelo <- subset(datos_def, select = c("SAP", "sexo", "edad", "altura", "peso", "niv_est","enf_cron","IMC","fuma", "sit_lab",
+                                             "frec_act_fis","estrato", "ingreso", "ingreso_eq","GHQ_12","m2","n_adultos","n_menores",
+                                             "n_dormitorios","ruido", "olores","agua","limpieza",
+                                             "cont_indus", "cont_otras","escasez_verde", "molest_animal", "delincuencia"))
+
+
+# Analisis descriptivo antes de pasar las variables de tipo numerico a tipo factor
+#library(summarytools)
+#t(descr(datos_modelo, stats = c("mean", "sd", "max", "min"), na.rm =TRUE))
+
+# Matriz de correlaciones
+#cor(datos_modelo,use = "complete.obs")
+
+# Convertimos algunas variables en tipo factor y las etiquetamos
+# Pasamos las variables categoricas a tipo factor
 datos_modelo$SAP <- factor(datos_modelo$SAP,labels=c("Buena salud","Mala salud"))
 datos_modelo$sexo <- factor(datos_modelo$sexo,labels=c("Mujer","Hombre"))
+datos_modelo$IMC <- factor(datos_modelo$IMC, labels=c("Si_saludable" , "No_saludable"))
 datos_modelo$niv_est <- factor(datos_modelo$niv_est,labels=c("Primaria","Secundaria","Bachillerato","Universidad"))
-datos_modelo$enf_cron <- factor(datos_modelo$enf_cron,labels=c("No","Sí"))
-datos_modelo$fuma <- factor(datos_modelo$fuma,labels=c("A diario","A veces","Antes sí","Nunca"))
-datos_modelo$sit_lab <- factor(datos_modelo$sit_lab,labels=c("Trabaja","desempleo","jubilado","estudiante","incapacitado","Hogar","Otros"))
-datos_modelo$frec_act_fis <- factor(datos_modelo$frec_act_fis,labels=c("No ejercicio","ocasional","Frec. mensual","Frec. Semanal"))
+datos_modelo$enf_cron <- factor(datos_modelo$enf_cron,labels=c("No","Si"))
+datos_modelo$GHQ_12<-factor(datos_modelo$GHQ_12,labels=c("No prob mental", "Si prob mental"))
+datos_modelo$fuma <- factor(datos_modelo$fuma,labels=c("Si","No"))
+datos_modelo$sit_lab <- factor(datos_modelo$sit_lab,labels=c("Si puede","No puede"))
+datos_modelo$frec_act_fis <- factor(datos_modelo$frec_act_fis,labels=c("No","Si"))
 
-etiquetas_hogar <- c("mucho","algo","nada")
+etiquetas_hogar <- c("No","Si")
+datos_modelo$ruido <- factor(datos_modelo$ruido, labels = etiquetas_hogar)
+datos_modelo$olores <- factor(datos_modelo$olores, labels = etiquetas_hogar)
+datos_modelo$agua <- factor(datos_modelo$agua, labels = etiquetas_hogar)
+datos_modelo$limpieza <- factor(datos_modelo$limpieza, labels = etiquetas_hogar)
+datos_modelo$cont_indus <- factor(datos_modelo$cont_indus, labels = etiquetas_hogar)
+datos_modelo$cont_otras <- factor(datos_modelo$cont_otras, labels = etiquetas_hogar)
+datos_modelo$escasez_verde <- factor(datos_modelo$escasez_verde, labels = etiquetas_hogar)
+datos_modelo$molest_animal <- factor(datos_modelo$molest_animal, labels = etiquetas_hogar)
+datos_modelo$delincuencia <- factor(datos_modelo$delincuencia, labels = etiquetas_hogar)
 
-# Convertimos algunas variables en tipo factor
-variables_factor <- c("SAP","sexo","niv_est","enf_cron","fuma","sit_lab","frec_act_fis","estrato",
-                     "ruido", "malos","agua","limpieza"  ,
-                     "cont_indus", "cont_otras","escasez_verde", "molest_animal", "delincuencia")
-
-datos_modelo[, variables_factor] <- lapply(datos_modelo[, variables_factor], as.factor)
 # Comprobamos
-class(datos_modelo$SAP)
-# Creamos el objeto que contiene los resultados del modelo
-modelo_logit <- glm(SAP ~ sexo + edad + niv_est + enf_cron + IMC + fuma + sit_lab +
-                       frec_act_fis + estrato  + ingreso_eq + GHQ_12 + m2 , 
-                       data = datos_modelo, family = binomial(link = "logit"),na.action = "na.omit")
+#class(datos_modelo$SAP)
+
+# Estimamos primero el modelo con las caracteristicas de la vivienda, para poder estimar el siguiente modelo con 
+# exactamente los mismos datos de esta primera estimación y poder llevar a cabo el ANOVA
+library(glm2)
+modelo_logit_viv <- glm2( SAP ~sexo + edad + IMC + niv_est + enf_cron + 
+                               sit_lab + frec_act_fis + GHQ_12  +  
+                               n_dormitorios + cont_otras + agua + ruido + delincuencia, 
+                         data = datos_modelo, family = binomial(link = "logit"),na.action = "na.omit")
 # Resultados
-summary(modelo_logit)
-plot(modelo_logit)
+summary(modelo_logit_viv)
+#plot(modelo_logit_viv)
 
 # Pseudo R2 (McFadden)
 modelo_nulo <- glm(SAP~1, family = "binomial",data = datos_modelo)
-summary(modelo_nulo)
-pseudo_R <- 1 - logLik(modelo_logit)/logLik(modelo_nulo) # 0.55
-pseudo_R
-coefs <- c(modelo_logit$coefficients)
-coefs
-
-#### M2: ELIMINAMOS LAS VARIABLES MENOS SIGNIFICATIVAS de M1 ####
+pseudo_R_viv_signif <- 1 - logLik(modelo_logit_viv)/logLik(modelo_nulo) 
+pseudo_R_viv_signif # 0.35071
 
 
-# Creamos el objeto que contiene los resultados del modelo
-modelo_logit_signif <- glm(SAP ~ edad + niv_est + enf_cron + sit_lab +frec_act_fis + ingreso_eq + GHQ_12, 
-                    data = datos_modelo, family = binomial(link = "logit"),na.action = "na.omit")
+
+# Modelo sin las caracteristicas de la vivienda empleando el dataframe del modelo con 
+# las caracteristicas de la vivienda
+modelo_logit <- glm2(SAP ~ sexo + edad + IMC + niv_est + enf_cron + 
+                          sit_lab + frec_act_fis + GHQ_12,
+                    data = modelo_logit_viv$model, family = binomial(link = "logit"),na.action = "na.omit")
+
 # Resultados
-summary(modelo_logit_signif)
+summary(modelo_logit)
+#plot(modelo_logit)
 
 # Pseudo R2 (McFadden)
-pseudo_R_signif <- 1 - logLik(modelo_logit_signif)/logLik(modelo_nulo) # 0.48
-pseudo_R_signif
-coefs_signif <- c(modelo_logit_signif$coefficients)
-coefs_signif
+pseudo_R <- 1 - logLik(modelo_logit)/logLik(modelo_nulo) 
+pseudo_R # 0.34956
+
+# Finalmente, para comparar ambos modelos, realizamos un ANOVA
+anova_noing <- anova(modelo_logit, modelo_logit_viv,test = "Chisq")
+anova_noing
 
 
-#### M3: COMO EL M1 PERO AHORA CONSIDERANDO LAS CARACTERISTICAS DE LA VIVIENDA ####
+# Vamos a analizar la bondad del ajuste de otra forma, calculando el porcentaje de aciertos del modelo
+
+# Modelo sin ingreso ni caracteristicas de la vivienda. 
+y_fitted_noing <- modelo_logit$fitted.values
+y_fitted_noing <- as.integer(y_fitted_noing >= 0.5)
+y_noing <- modelo_logit$y
+predict_eval_noing <- y_fitted_noing == y_noing
+tabla_noing <- table(predict_eval_noing)
+tabla_noing[2]/(tabla_noing[1] + tabla_noing[2]) # 0.78229
+
+# Modelo sin ingreso y con caracteristicas de la vivienda
+# Este es el que tiene mayor porcentaje de aciertos
+y_fitted_noing_viv <- modelo_logit_viv$fitted.values
+y_fitted_noing_viv <- as.integer(y_fitted_noing_viv >= 0.5)
+y_noing_viv <- modelo_logit_viv$y
+predict_eval_noing_viv <- y_fitted_noing_viv == y_noing_viv
+tabla_noing_viv <- table(predict_eval_noing_viv)
+tabla_noing_viv[2]/(tabla_noing_viv[1] + tabla_noing_viv[2]) # 0.78336
 
 
-# Creamos el objeto que contiene los resultados del modelo
-modelo_logit_viv <- glm(SAP ~ edad  + enf_cron + IMC + fuma + sit_lab +frec_act_fis  +GHQ_12 + m2 +
-                        ruido + malos + agua + limpieza + cont_indus + cont_otras + escasez_verde + molest_animal + delincuencia, 
-                    data = datos_modelo, family = binomial(link = "logit"),na.action = "na.omit")
-# Resultados
-summary(modelo_logit_viv)
+# Curva de ROC
 
-# Pseudo R2 (McFadden)
+library(pROC)
 
+# Modelo con vivienda y sin ingreso
+curva_ROC_viv = roc(modelo_logit_viv$y,modelo_logit_viv$fitted.values)
+p_viv <- plot(curva_ROC_viv,
+          col = "blue",
+          print.auc = TRUE,
+          print.auc.x = 0.8,
+          print.auc.y = 0.6, 
+          xlim = c(1,0),
+          ylim = c(0,1),
+          xlab = "1 - Especifidad", 
+          ylab = "Sensibilidad", 
+          main = "Curva de ROC Modelo II",
+          print.auc.pattern = "%f")
 
-pseudo_R_viv <- 1 - logLik(modelo_logit_viv)/logLik(modelo_nulo) # 0.55
-pseudo_R_viv
-coefs_viv <- c(modelo_logit_viv$coefficients)
-coefs_viv
+polygon(with(p_viv, cbind(specificities, sensitivities)), 
+        col = rgb(.25,0.31,0.61, alpha = 0.4), 
+        border = "blue",
+        lwd = 2)
 
+# Modelo sin vivienda ni ingreso
+curva_ROC = roc(modelo_logit$y,modelo_logit$fitted.values)
+p <- plot(curva_ROC,
+          col = "blue",
+          print.auc = TRUE,
+          print.auc.x = 0.8,
+          print.auc.y = 0.6, 
+          xlim = c(1,0),
+          ylim = c(0,1),
+          xlab = "1 - Especifidad", 
+          ylab = "Sensibilidad", 
+          main = "Curva de ROC Modelo I",
+          print.auc.pattern = "%f")
 
-
-#### M4: COMO EL M2 PERO CONSIDERANDO LAS CARACTERISTICAS DE LA VIVIENDA ####
-
-
-# Creamos el objeto que contiene los resultados del modelo
-modelo_logit_viv_signif <- glm( SAP ~sexo + edad + niv_est + enf_cron + IMC + fuma + sit_lab +
-                                  frec_act_fis + estrato  + ingreso_eq + GHQ_12 + m2  + 
-                          ruido + malos + agua + limpieza + cont_indus + cont_otras + escasez_verde + molest_animal + delincuencia, 
-                        data = datos_modelo, family = binomial(link = "logit"),na.action = "na.omit")
-# Resultados
-summary(modelo_logit_viv_signif)
-plot(modelo_logit_signif)
-
-# Pseudo R2 (McFadden)
-pseudo_R_viv_signif <- 1 - logLik(modelo_logit_viv_signif)/logLik(modelo_nulo) # 0.49
-pseudo_R_viv_signif
-coefs_viv_signif <- c(modelo_logit_viv_signif$coefficients)
-coefs_viv_signif
-
-#### Análisis descriptivo de M4 ####
-
-library(tidyverse)
-library(skimr)
-library(broom)
-
-# mtcars %>% 
-#   as_tibble() %>% 
-#   mutate_at(vars(gear), factor) %>% 
-#   select(mpg, wt, gear, cyl) -> limpio
-# 
-# skim(limpio) %>% 
-#   as_tibble() -> descriptivos
-# 
-# 
-# modelo <- glm(gear~mpg+wt+cyl, family = binomial(), data = limpio)
-# tidy(modelo) %>% 
-#   rename(skim_variable = term) %>% #para que coincida el nombre en el join
-#   inner_join(descriptivos)  %>% 
-#   select(variable = skim_variable, 
-#          coeficiente = estimate, 
-#          error = std.error, 
-#          media = numeric.mean,
-#          desviación = numeric.sd
-#   )
-
-
-
-
-library(tidyverse)
-library(skimr)
-library(broom)
-
-
-library(broom)
-
-datos_modelo %>%
-  as_tibble() %>%
-  mutate_at(vars(SAP), factor) %>%
-  select(SAP, sexo, edad, niv_est, enf_cron, IMC, fuma, sit_lab, frec_act_fis, estrato, ingreso_eq, GHQ_12, m2,
-         ruido, malos, agua, limpieza, cont_indus, cont_otras, escasez_verde, molest_animal, delincuencia) -> limpio
-
-skim(limpio) %>%
-  as_tibble() -> descriptivos
-
-
-modelo <-glm( SAP ~sexo + edad + niv_est + enf_cron + IMC + fuma + sit_lab +
-                frec_act_fis + estrato  + ingreso_eq + GHQ_12 + m2  + 
-                ruido + malos + agua + limpieza + cont_indus + cont_otras + escasez_verde + molest_animal + delincuencia, 
-              data = datos_modelo, family = binomial(link = "logit"),na.action = "na.omit")
-tidy(modelo) %>%
-  rename(skim_variable = term) %>% #para que coincida el nombre en el join
-  inner_join(descriptivos)  %>%
-  select(variable = skim_variable,
-         coeficiente = estimate,
-         error = std.error,
-         media = numeric.mean,
-         desviación = numeric.sd,
-         mínimo = numeric.p0,
-         máximo = numeric.p100
-  )
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+polygon(with(p, cbind(specificities, sensitivities)), 
+        col = rgb(.25,0.31,0.61, alpha = 0.4), 
+        border = "blue",
+        lwd = 2)
--- model_2.R
+++ model_2.R
@@ -1,49 +1,43 @@
+#### PAQUETES USADOS####
+
+#install.packages("readxl")
+#install.packages("mlogit")
+#install.packages("glm2")
+
+
 library(readxl)
 datos_def <- read_excel("datos_def_excel")
+names(datos_def)
+summary(datos_def)
+#options(digits = 4,scipen = 5)
 
+# Introducimos las variables de interes un objeto subset para que puedan introducirse en el objeto glm
 datos_modelo <- subset(datos_def, select = c("SAP", "sexo", "edad", "altura", "peso", "niv_est","enf_cron","IMC","fuma", "sit_lab",
                                              "frec_act_fis","estrato", "ingreso", "ingreso_eq","GHQ_12","m2","n_adultos","n_menores",
                                              "n_dormitorios","ruido", "olores","agua","limpieza",
                                              "cont_indus", "cont_otras","escasez_verde", "molest_animal", "delincuencia"))
 
 
-# Vamos a agrupar los valores  de algunas variables (convertirlas en binarias)
-# para que mejoren su significatividad en el modelo: fuma; frec_act_fis; caract. vivienda
-
-attach(datos_modelo)
-datos_modelo$frec_act_fis[frec_act_fis == 1 | frec_act_fis == 2] <- 1 # No hace ejercicio
-datos_modelo$frec_act_fis[frec_act_fis == 3 | frec_act_fis == 4] <- 0 # Sí hace ejercicio
-table(datos_modelo$frec_act_fis)
-
-datos_modelo$sit_lab[sit_lab == 1 | sit_lab == 2 | sit_lab == 3 | sit_lab == 4 | sit_lab == 6 | sit_lab == 7] <- 0 # Si trabaja
-datos_modelo$sit_lab[sit_lab == 5] <- 1 # No trabaja
-
-datos_modelo$fuma[fuma == 1 | fuma == 2] <- 1 # Si fuma
-datos_modelo$fuma[fuma == 3 | fuma == 4] <- 0 # No fuma
-table(datos_modelo$fuma)
-
-seleccion <- c("ruido", "olores","agua","limpieza", "cont_indus", "cont_otras",
-             "escasez_verde", "molest_animal", "delincuencia")
-datos_modelo[, seleccion] <- datos_modelo[, seleccion] * (datos_modelo[, seleccion] == 1)
-table(datos_modelo$olores)
-
 # Analisis descriptivo antes de pasar las variables de tipo numerico a tipo factor
-library(summarytools)
-t(descr(datos_modelo, stats = c("mean", "sd", "max", "min","n.valid"), na.rm = TRUE))
+#library(summarytools)
+#t(descr(datos_modelo, stats = c("mean", "sd", "min", "max","n.valid"), na.rm = TRUE))
 
 # Matriz de correlaciones
-cor(datos_modelo,use = "complete.obs")
+#cor(datos_modelo,use = "complete.obs")
 
+# Convertimos algunas variables en tipo factor y las etiquetamos
 # Pasamos las variables categoricas a tipo factor
 datos_modelo$SAP <- factor(datos_modelo$SAP,labels=c("Buena salud","Mala salud"))
 datos_modelo$sexo <- factor(datos_modelo$sexo,labels=c("Mujer","Hombre"))
+datos_modelo$IMC <- factor(datos_modelo$IMC, labels=c("Si saludable, No saludable"))
 datos_modelo$niv_est <- factor(datos_modelo$niv_est,labels=c("Primaria","Secundaria","Bachillerato","Universidad"))
-datos_modelo$enf_cron <- factor(datos_modelo$enf_cron,labels=c("No","Sí"))
-datos_modelo$fuma <- factor(datos_modelo$fuma,labels=c("Sí","No"))
+datos_modelo$enf_cron <- factor(datos_modelo$enf_cron,labels=c("No","Si"))
+datos_modelo$GHQ_12<-factor(datos_modelo$GHQ_12,labels=c("No prob mental", "Si prob mental"))
+datos_modelo$fuma <- factor(datos_modelo$fuma,labels=c("Si","No"))
 datos_modelo$sit_lab <- factor(datos_modelo$sit_lab,labels=c("Si puede","No puede"))
-datos_modelo$frec_act_fis <- factor(datos_modelo$frec_act_fis,labels=c("No","Sí"))
+datos_modelo$frec_act_fis <- factor(datos_modelo$frec_act_fis,labels=c("No","Si"))
 
-etiquetas_hogar <- c("Sí","No")
+etiquetas_hogar <- c("No","Si")
 datos_modelo$ruido <- factor(datos_modelo$ruido, labels = etiquetas_hogar)
 datos_modelo$olores <- factor(datos_modelo$olores, labels = etiquetas_hogar)
 datos_modelo$agua <- factor(datos_modelo$agua, labels = etiquetas_hogar)
@@ -54,20 +48,99 @@ datos_modelo$escasez_verde <- factor(datos_modelo$escasez_verde, labels = etique
 datos_modelo$molest_animal <- factor(datos_modelo$molest_animal, labels = etiquetas_hogar)
 datos_modelo$delincuencia <- factor(datos_modelo$delincuencia, labels = etiquetas_hogar)
 
-class(datos_modelo$SAP); class(datos_modelo$agua)
+# Comprobamos
+#class(datos_modelo$SAP); class(datos_modelo$agua)
 
-# Estimacion sin caracteristicas/entorno de la vivienda
-modelo_logit <- glm(SAP ~ sexo + edad + peso + niv_est + enf_cron   + sit_lab + 
-                      frec_act_fis   + ingreso_eq + GHQ_12 , 
-                    data = datos_modelo, family = binomial(link = "logit"),na.action = "na.omit")
-
-summary.glm(modelo_logit)
-
-# Añadimos las características de la vivienda una a una para evitar posible multicolinealidad
-modelo_logit_viv <- glm( SAP ~ sexo + edad + peso + niv_est + enf_cron   + sit_lab +
-                               frec_act_fis   + ingreso_eq + GHQ_12 +  
-                               n_dormitorios  + delincuencia + cont_otras,
+# Estimamos primero el modelo con las caracteristicas de la vivienda, para poder estimar el siguiente modelo con 
+# exactamente los mismos datos de esta primera estimación y poder llevar a cabo el ANOVA
+library(glm2)
+modelo_logit_ing_viv <- glm2( SAP ~ sexo + edad + IMC + niv_est + enf_cron + 
+                                    sit_lab + frec_act_fis + GHQ_12 + ingreso_eq +
+                                    n_dormitorios  + delincuencia  + cont_otras,
                          data = datos_modelo, family = binomial(link = "logit"),na.action = "na.omit")
 # Resultados
-summary(modelo_logit_viv)
+summary(modelo_logit_ing_viv)
+#plot(modelo_logit_viv)
 
+# Pseudo R2 McFadden
+# Este es el que presenta un mejor Pseudo R2 McFadden
+modelo_nulo <- glm(SAP~1, family = "binomial",data = datos_modelo)
+pseudo_R_ing_viv <- 1 - logLik(modelo_logit_ing_viv)/logLik(modelo_nulo) 
+pseudo_R_ing_viv #0.48565
+
+
+modelo_logit_ing <- glm2(SAP ~ sexo + edad + IMC + niv_est + enf_cron  +  
+                               sit_lab + frec_act_fis + GHQ_12 + ingreso_eq , 
+                    data = modelo_logit_ing_viv$model, family = binomial(link = "logit"),na.action = "na.omit")
+# Resultados
+summary.glm(modelo_logit_ing)
+#plot(modelo_logit)
+
+# Pseudo R2 McFadden
+pseudo_R_viv <- 1 - logLik(modelo_logit_ing)/logLik(modelo_nulo) 
+pseudo_R_viv # 0.48489
+
+anova_ing <- anova(modelo_logit_ing, modelo_logit_ing_viv, test = "Chisq")
+anova_ing
+
+# Vamos a analizar la bondad del ajuste de otra forma, calculando el porcentaje de aciertos del modelo
+
+# Modelo sin ingreso ni caracteristicas de la vivienda
+y_fitted_ing <- modelo_logit_ing$fitted.values
+y_fitted_ing <- as.integer(y_fitted_ing >= 0.5)
+y_ing <- modelo_logit_ing$y
+predict_eval_ing <- y_fitted_ing == y_ing
+tabla_ing <- table(predict_eval_ing)
+tabla_ing[2]/(tabla_ing[1] + tabla_ing[2]) # 0.7741
+
+# Modelo sin ingreso y con caracteristicas de la vivienda
+y_fitted_ing_viv <- modelo_logit_ing_viv$fitted.values
+y_fitted_ing_viv <- as.integer(y_fitted_ing_viv >= 0.5)
+y_ing_viv <- modelo_logit_ing_viv$y
+predict_eval_ing_viv <- y_fitted_ing_viv == y_ing_viv
+tabla_ing_viv <- table(predict_eval_ing_viv)
+tabla_ing_viv[2]/(tabla_ing_viv[1] + tabla_ing_viv[2]) # 0.77556
+
+
+# Curva de ROC
+
+library(pROC)
+
+# Modelo sin vivienda y con ingreso
+curva_ROC_ing = roc(modelo_logit_ing$y,modelo_logit_ing$fitted.values)
+p_ing <- plot(curva_ROC_ing,
+              col = "blue",
+              print.auc = TRUE,
+              print.auc.x = 0.8,
+              print.auc.y = 0.6, 
+              xlim = c(1,0),
+              ylim = c(0,1),
+              xlab = "1 - Especifidad", 
+              ylab = "Sensibilidad", 
+              main = "Curva de ROC Modelo III",
+              print.auc.pattern = "%f")
+
+polygon(with(p_ing, cbind(specificities, sensitivities)), 
+        col = rgb(.25,0.31,0.61, alpha = 0.4), 
+        border = "blue",
+        lwd = 2)
+
+
+# Modelo con vivienda y con ingreso
+curva_ROC_ing_viv = roc(modelo_logit_ing_viv$y,modelo_logit_ing_viv$fitted.values)
+p_ing_viv <- plot(curva_ROC_ing_viv,
+              col = "blue",
+              print.auc = TRUE,
+              print.auc.x = 0.8,
+              print.auc.y = 0.6, 
+              xlim = c(1,0),
+              ylim = c(0,1),
+              xlab = "1 - Especifidad", 
+              ylab = "Sensibilidad", 
+              main = "Curva de ROC Modelo IV",
+              print.auc.pattern = "%f")
+
+polygon(with(p_ing_viv, cbind(specificities, sensitivities)), 
+        col = rgb(.25,0.31,0.61, alpha = 0.4), 
+        border = "blue",
+        lwd = 2)
